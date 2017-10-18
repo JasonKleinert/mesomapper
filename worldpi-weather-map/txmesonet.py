55d9c8074680e31f5b4ea30680c0f7f6eb4f5b9c
Jason Kleinert <jason.kleinert@gmail.com>
2017-10-18 12:16:07 -0500

    adding geoserver pi and multi layers

--- worldpi-weather-map/txmesonet.py
+++ worldpi-weather-map/txmesonet.py
@@ -6,36 +6,55 @@ import webbrowser
 
 # get weather station data
 txmeso_url = 'https://www.texmesonet.org/api/CurrentData'
-url = 'http://api.mesowest.net/v2/stations/metadata?&state=tx&county=Travis&token=demotoken'
-data = get(url).json()
+mesowest_url = 'http://api.mesowest.net/v2/stations/metadata?&state=tx&county=Travis&token=demotoken'
+mesowest_data = get(mesowest_url).json()
 txmeso_data = get(txmeso_url).json()
+
 lons = []
 lats = []
 wsnames = []
+airtemp = []
+
 lons = [station['longitude'] for station in txmeso_data]
 lats = [station['latitude'] for station in txmeso_data]
 wsnames = [station['name'] for station in txmeso_data]
+airtemp = [station['airTemp'] for station in txmeso_data]
 
 # initialize the map
-map_ws = folium.Map(location=[30.334694,-97.781949],zoom_start=10, tiles='Stamen Terrain')
+m = folium.Map(location=[30.334694,-97.781949],zoom_start=10, tiles='Stamen Terrain')
+
+# add layers from geoserver pi
+txdot_counties_detailed = get('http://10.10.11.54:8080/geoserver/tnris/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=tnris:txdot-2015-county-generalized_tx&maxFeatures=254&outputFormat=application%2Fjson').json()
+folium.GeoJson(
+    txdot_counties_detailed,
+    name='Counties'
+).add_to(m)
+
+txdot_counties_detailed = get('http://10.10.11.54:8080/geoserver/tnris/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=tnris:txdot_2015_county_detailed_tx&maxFeatures=254&outputFormat=application%2Fjson').json()
+folium.GeoJson(
+    txdot_counties_detailed,
+    name='Counties Detailed'
+).add_to(m)
+
+folium.LayerControl().add_to(m)
 
 # add the mesonet weather stations to the map
-for station in data['STATION']:
+for station in mesowest_data['STATION']:
     if station['STATUS'] == 'ACTIVE':
         tooltip = station['NAME']
         folium.Marker([float(station['LATITUDE']),
                        float(station['LONGITUDE'])],
                        popup=tooltip,
-                       icon=folium.Icon(icon = 'cloud')).add_to(map_ws)
+                       icon=folium.Icon(icon = 'cloud')).add_to(m)
 
 # add the texmesonet weather stations to the map
 for n in range(len(lons)):
     folium.Marker([lats[n],
                    lons[n]],
-                   popup=wsnames[n],
-                   icon=folium.Icon(icon = 'cloud', color = 'pink')).add_to(map_ws)
+                   popup=wsnames[n] + '\n' + str(airtemp[n]),
+                   icon=folium.Icon(icon = 'cloud', color = 'pink')).add_to(m)
 
 # save the map as html and open in the browser
 CWD = os.getcwd()
-map_ws.save('mesomap.html')
+m.save('mesomap.html')
 webbrowser.open_new('file://' + CWD + '/' + 'mesomap.html')
